928ab25c875204b1ace60ad6dd94a3ca8db72eb8
Ayman-Waid <127792112+Ayman-Waid@users.noreply.github.com>
2024-03-28 00:45:34 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 def find_peak(list_of_integers):
-    if not list_of_integers:
+    """Finds a peak in list_of_integers"""
+
+    if list_of_integers is None or list_of_integers == []:
         return None
 
-    def helper(nums, low, high):
-        if low == high:
-            return nums[low]
+    lo = 0
+    hi = len(list_of_integers) - 1  # Subtract 1 to adjust for 0-based indexing
 
-        mid = (low + high) // 2
+    while lo < hi:
+        mid = (lo + hi) // 2
 
-        if nums[mid] > nums[mid + 1]:
-            return helper(nums, low, mid)
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            lo = mid + 1
         else:
-            return helper(nums, mid + 1, high)
+            hi = mid
 
-    return helper(list_of_integers, 0, len(list_of_integers) - 1)
+    return list_of_integers[lo]
 
