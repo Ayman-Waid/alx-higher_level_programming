6454c323cdb2476e7c751d81d816242d47b04ca2
Ayman-Waid <127792112+Ayman-Waid@users.noreply.github.com>
2024-03-28 00:44:31 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-"""Finds a peak in a list of unsorted integers"""
-
-
 def find_peak(list_of_integers):
-    """Finds a peak in list_of_integers"""
-
-    if list_of_integers is None or list_of_integers == []:
+    if not list_of_integers:
         return None
-    lo = 0
-    hi = len(list_of_integers)
-    mid = ((hi - lo) // 2) + lo
-    mid = int(mid)
-    if hi == 1:
-        return list_of_integers[0]
-    if hi == 2:
-        return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-            list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:])
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+
+    def helper(nums, low, high):
+        if low == high:
+            return nums[low]
+
+        mid = (low + high) // 2
+
+        if nums[mid] > nums[mid + 1]:
+            return helper(nums, low, mid)
+        else:
+            return helper(nums, mid + 1, high)
+
+    return helper(list_of_integers, 0, len(list_of_integers) - 1)
+
